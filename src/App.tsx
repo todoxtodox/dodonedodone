a6eb6b7e4f5e266476fd479545a5f435fee16a62
Dongmin Ahn <eamon3481@naver.com>
2023-02-26 22:21:05 +0900

    chore: add eslint config and .prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   const [count, setCount] = React.useState(0);
-      
-       return <div className="App"></div>;
+
+  return <div className="App"></div>;
 }
 
 export default App;
