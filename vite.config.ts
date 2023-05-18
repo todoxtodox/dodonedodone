dc9b8a5fa79cabd19570175d73b4e7bdf09a3e81
Chloé <chloeelog@gmail.com>
2023-05-18 20:22:59 +0900

    fix: build error during importing path at vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
