aa908f424f2aa167709cde26665cb146c130bcc4
Chloé <chloeelog@gmail.com>
2023-05-18 20:13:36 +0900

    feat: add vanilla-extract/css

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,125 +1,119 @@
 module.exports = {
-    root: true,
-    env: {
-        es6: true,
-        node: true,
-        browser: true,
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+    'plugin:react/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'import', 'prettier', 'react', 'react-hooks'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'eslint-config-prettier',
-        'plugin:react/recommended',
+    jsx: true,
+    useJSXTextNode: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    indent: 'off',
+    semi: 'off',
+    'prefer-const': 'error',
+    'getter-return': 'warn',
+    curly: ['error', 'all'],
+    eqeqeq: ['error', 'always', { null: 'ignore' }],
+    'no-implicit-coercion': 'error',
+    'no-undef': 'off',
+    'no-extra-boolean-cast': 'off',
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'util',
+            importNames: ['isArray'],
+            message: '`Array.isArray`를 대신 사용해주세요!',
+          },
+        ],
+      },
     ],
-    plugins: [ 
-        '@typescript-eslint',
-        'import',
-        'prettier',
-        'react',
-        'react-hooks',
+    'no-async-promise-executor': 'warn',
+    'no-warning-comments': [
+      'warn',
+      {
+        terms: ['TODO', 'FIXME', 'XXX', 'BUG'],
+        location: 'anywhere',
+      },
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
-        jsx: true,
-        useJSXTextNode: true,
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-        'import/resolver': {
-            typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
-        },
-    },
-    rules: {
-        'prettier/prettier': 'error',
-        indent: 'off',
-        semi: 'off',
-        'prefer-const': 'error',
-        'getter-return': 'warn',
-        curly: ['error', 'all'],
-        eqeqeq: ['error', 'always', { null: 'ignore' }],
-        'no-implicit-coercion': 'error',
-        'no-undef': 'off',
-        'no-extra-boolean-cast': 'off',
-        'no-restricted-imports': [
-            'error',
-            {
-                paths: [
-                    {
-                        name: 'util',
-                        importNames: ['isArray'],
-                        message: '`Array.isArray`를 대신 사용해주세요!',
-                    },
-                ],
-            },
+    'no-var': 'error',
+    'import/no-duplicates': 'error',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/prefer-as-const': 'warn',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
+    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+        selector: 'variable',
+        leadingUnderscore: 'allow',
+      },
+      { format: ['camelCase', 'PascalCase'], selector: 'function' },
+      { format: ['PascalCase'], selector: 'interface' },
+      { format: ['PascalCase'], selector: 'typeAlias' },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { ignoreRestSiblings: true },
+    ],
+    '@typescript-eslint/no-var-requires': 'warn',
+    '@typescript-eslint/member-ordering': [
+      'error',
+      {
+        default: [
+          'public-static-field',
+          'private-static-field',
+          'public-instance-field',
+          'private-instance-field',
+          'public-constructor',
+          'private-constructor',
+          'public-instance-method',
+          'private-instance-method',
         ],
-        'no-async-promise-executor': 'warn',
-        'no-warning-comments': [
-            'warn',
-            {
-                terms: ['TODO', 'FIXME', 'XXX', 'BUG'],
-                location: 'anywhere',
-            },
-        ],
-        'no-var': 'error',
-        'import/no-duplicates': 'error',
-        '@typescript-eslint/indent': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-use-before-define': 'off',
-        '@typescript-eslint/no-empty-interface': 'off',
-        '@typescript-eslint/no-parameter-properties': 'off',
-        '@typescript-eslint/prefer-as-const': 'warn',
-        '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
-        '@typescript-eslint/ban-types': 'warn',
-        '@typescript-eslint/no-inferrable-types': 'warn',
-        '@typescript-eslint/no-empty-function': 'off',
-        '@typescript-eslint/naming-convention': [
-            'error',
-            {
-                format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
-                selector: 'variable',
-                leadingUnderscore: 'allow',
-            },
-            { format: ['camelCase', 'PascalCase'], selector: 'function' },
-            { format: ['PascalCase'], selector: 'interface' },
-            { format: ['PascalCase'], selector: 'typeAlias' },
-        ],
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
-        '@typescript-eslint/no-unused-vars': [
-            'error',
-            { ignoreRestSiblings: true },
-        ],
-        '@typescript-eslint/no-var-requires': 'warn',
-        '@typescript-eslint/member-ordering': [
-            'error',
-            {
-                default: [
-                    'public-static-field',
-                    'private-static-field',
-                    'public-instance-field',
-                    'private-instance-field',
-                    'public-constructor',
-                    'private-constructor',
-                    'public-instance-method',
-                    'private-instance-method',
-                ],
-            },
-        ],
-        //
-        'react/prop-types': 'off',
-        'react/display-name': 'off',
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'error',
-        'react/jsx-no-target-blank': 'error',
-        'react/react-in-jsx-scope': 'off',
-    },
+      },
+    ],
+    //
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'react/jsx-no-target-blank': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
 };
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # dodonedodone
 
 두던두던 web-prototype by team.todoxtodox
- 
